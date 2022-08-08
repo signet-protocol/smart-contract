3beac697987bf4dcfa6939766b757b30cff9cb9c
kopy-kat <kopp.konrad@gmail.com>
2022-08-08 14:25:32 +0200

    smart contract init

---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+const ethers = require('ethers');
+require('dotenv').config();
+
+async function main() {
+
+  const url = process.env.MUMBAI_URL;
+
+  // woah, we just cut out the whole compile.js flow with this!
+  let artifacts = await hre.artifacts.readArtifact("RTSProxyWallet");
+
+  const provider = new ethers.providers.JsonRpcProvider(url);
+
+  let privateKey = process.env.PRIVATE_KEY;
+
+  let wallet = new ethers.Wallet(privateKey, provider);
+
+  let factory = new ethers.ContractFactory(artifacts.abi, artifacts.bytecode, wallet);
+
+  let permissionsContract = await factory.deploy();
+
+  console.log("Contract address:", permissionsContract.address);
+
+  await permissionsContract.deployed();
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+});
